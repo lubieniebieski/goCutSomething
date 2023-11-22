5b926228a0fcb6742e8531df403377f405970667
Adam Nowak <lubieniebieski@gmail.com>
2023-11-22 21:47:30 +0100

    how to cut wood

---
+++ main.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
+type CutDimensions struct {
+	Diameter int
+	Spacing  int
+	Pieces   int
+}
+
+func (c CutDimensions) areNice() bool {
+	return math.Mod(float64(c.getDistanceBetweenCenters()), 10.0) == 0
+}
+
+func (c CutDimensions) getDistanceBetweenCenters() int {
+	return c.Spacing + c.Diameter
+}
+
+func generateIntegers(desiredInt int, count int) []int {
+	result := make([]int, count*2+1)
+
+	for i := 0; i < (count*2 + 1); i++ {
+		result[i] = desiredInt - count + i
+	}
+	return result
+
+}
+
+func cuttableWithoutMargin(materialWidth int, diameter int, spacing int) bool {
+	return math.Mod(float64(materialWidth-diameter), float64(diameter+spacing)) == 0
+}
+
+func possibleCutDimensions(materialWidth int, desiredDiameter int, desiredSpacing int) (result []CutDimensions) {
+
+	possibleSpacings := generateIntegers(desiredSpacing, 100)
+	possibleDiameters := generateIntegers(desiredDiameter, 100)
+	for _, spacing := range possibleSpacings {
+		for _, diameter := range possibleDiameters {
+			if spacing < 1 || diameter < 1 {
+				continue
+			}
+			if cuttableWithoutMargin(materialWidth, diameter, spacing) {
+				newDimensions := CutDimensions{Diameter: diameter, Spacing: spacing}
+				newDimensions.Pieces = (materialWidth - diameter) / (diameter + spacing)
+				if newDimensions.areNice() {
+					result = append(result, newDimensions)
+				}
+			}
+		}
+	}
+	if len(result) > 0 {
+		return result
+	}
+
+	println("returning empty")
+	return []CutDimensions{}
+
+}
+
+func main() {
+	// Check if the correct number of arguments is provided
+	if len(os.Args) != 4 {
+		fmt.Println("Usage: go run main.go <width> <margin> <diameter>")
+		return
+	}
+
+	// Parse the arguments
+	width, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Println("Invalid width:", os.Args[1])
+		return
+	}
+
+	margin, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		fmt.Println("Invalid margin:", os.Args[2])
+		return
+	}
+
+	desiredDiameter, err := strconv.Atoi(os.Args[3])
+	if err != nil {
+		fmt.Println("Invalid diameter:", os.Args[3])
+		return
+	}
+	availableLength := width - margin
+	// Print the values
+	fmt.Println("Width:", width, "mm")
+	fmt.Println("Margin:", margin, "mm")
+	fmt.Println("Available length: ", availableLength, "mm")
+	desiredSpacing := 100
+
+	possibilities := possibleCutDimensions(availableLength, desiredDiameter, desiredSpacing)
+	for _, possibility := range possibilities {
+
+		fmt.Printf("You can drill %d holes with the folllowing parameters:\n", possibility.Pieces)
+		fmt.Printf("\tDrill ⌀: %dmm\tdistance : %dmm\n", possibility.Diameter, possibility.getDistanceBetweenCenters())
+	}
+	printAsCSV(possibilities)
+	printToGoogleSheets(possibilities)
+
+}
+
+func printAsCSV(cuts []CutDimensions) {
+	fmt.Println("Drill diameters,Distance betwen points,Pieces")
+	for _, cut := range cuts {
+		fmt.Printf("%d,%d,%d\n", cut.Diameter, cut.getDistanceBetweenCenters(), cut.Pieces)
+	}
+}
+
+func printToGoogleSheets(cuts []CutDimensions) {
+	fmt.Println("Drill diameters\tDistance betwen points\tPieces")
+	for _, cut := range cuts {
+		fmt.Printf("%d\t%d\t%d\n", cut.Diameter, cut.getDistanceBetweenCenters(), cut.Pieces)
+	}
+}
---
+++ main_test.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"slices"
+	"testing"
+)
+
+func TestGenerateIntegers(t *testing.T) {
+	desiredInt := 100
+	count := 5
+	expected := []int{
+		95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105,
+	}
+
+	got := generateIntegers(desiredInt, count)
+	if !reflect.DeepEqual(got, expected) {
+		t.Errorf("generateIntegers(%d) == %v, want %v", desiredInt, got, expected)
+	}
+}
+
+func TestCuttableWithoutMargin(t *testing.T) {
+	materialWidth := 1000
+	diameter := 100
+	spacing := 50
+	expected := true
+
+	got := cuttableWithoutMargin(materialWidth, diameter, spacing)
+	if got != expected {
+		t.Errorf("cuttableWithoutMargin(%d, %d, %d) == %v, want %v", materialWidth, diameter, spacing, got, expected)
+	}
+
+	materialWidth = 1000
+	diameter = 100
+	spacing = 60
+	expected = false
+
+	got = cuttableWithoutMargin(materialWidth, diameter, spacing)
+	if got != expected {
+		t.Errorf("cuttableWithoutMargin(%d, %d, %d) == %v, want %v", materialWidth, diameter, spacing, got, expected)
+	}
+}
+
+func TestPossibleCutDimensions(t *testing.T) {
+	cases := []struct {
+		materialWidth int
+		diameter      int
+		expected      []CutDimensions
+	}{
+		{100, 40, []CutDimensions{{Diameter: 40, Spacing: 20, Pieces: 1}}},
+		{900, 100, []CutDimensions{{Diameter: 100, Spacing: 60, Pieces: 5}, {Diameter: 100, Spacing: 100, Pieces: 4}}},
+		{800, 200, []CutDimensions{{Diameter: 200, Spacing: 100, Pieces: 2}}},
+		{1112, 12, []CutDimensions{{Diameter: 12, Spacing: 88, Pieces: 11}}},
+	}
+
+	for i, c := range cases {
+		name := fmt.Sprintf("Case #%d", i)
+		t.Run(name, func(t *testing.T) {
+			assertCuts(t, c.materialWidth, c.diameter, c.expected)
+		})
+	}
+}
+
+func assertCuts(t *testing.T, materialWidth int, diameter int, expected []CutDimensions) {
+	t.Helper()
+	got := possibleCutDimensions(materialWidth, diameter, 10)
+	for _, e := range expected {
+		if slices.Contains(got, e) == false {
+			t.Errorf("it should contain %v but it didn't", e)
+			t.Errorf("possibleCutDimensions(%d, %d) == %v, want %v", materialWidth, diameter, got, expected)
+		}
+	}
+}
